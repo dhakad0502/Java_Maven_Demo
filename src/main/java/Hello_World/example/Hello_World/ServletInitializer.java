16cf5f5d741ba639c5cdc779ac2b3d17c8352238
unknown <dhakad0502@gmail.com>
2022-08-09 16:48:10 +0530

    first commiit for maven demo project

---
+++ src/main/java/Hello_World/example/Hello_World/ServletInitializer.java
@@ -0,0 +1,13 @@
+package Hello_World.example.Hello_World;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HelloWorldApplication.class);
+	}
+
+}
